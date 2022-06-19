f244b2dbaf259b31baeaa9f4c438e8ea1da83a44
safietou2022 <safietousarr1998@gmail.com>
2022-06-19 22:33:52 +0000

    conception de dictionnaire

---
+++ verification.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+if(isset($_POST['username']) && isset($_POST['password']))
+{
+    // connexion à la base de données
+    $db_username = 'root';
+    $db_password = '';
+    $db_name     = 'dictionnaire';
+    $db_host     = 'localhost';
+    $db = mysqli_connect($db_host, $db_username, $db_password,$db_name)
+           or die("vous n'êtes pas connecter à la base de données");
+    
+    
+    $username = mysqli_real_escape_string($db,htmlspecialchars($_POST['username'])); 
+    $password = mysqli_real_escape_string($db,htmlspecialchars($_POST['password']));
+    
+    if($username !== "" && $password !== "")
+    {
+        $requete = "SELECT count(*) FROM donnees where 
+              username = '".$username."' and password = '".$password."' ";
+        $exec_requete = mysqli_query($db,$requete);
+        $reponse      = mysqli_fetch_array($exec_requete);
+        $count = $reponse['count(*)'];
+        if($count!=0) // nom d'utilisateur et mot de passe correctes
+        {
+           $_SESSION['username'] = $username;
+           header('Location: index1.php');
+        }
+        else
+        {
+           header('Location: login.php?erreur=1'); // utilisateur ou mot de passe incorrect
+        }
+    }
+    else
+    {
+       header('Location: login.php?erreur=2'); // utilisateur ou mot de passe vide
+    }
+}
+else
+{
+   header('Location: login.php');
+}
+mysqli_close($db); // fermer la connexion
+?>
